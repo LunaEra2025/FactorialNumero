23c68539506e599cc9cdd544cf7a7a0f9a9cd7f2
luna <erahin.pl@voaxaca.tecnm.mx>
2026-09-07 10:40:29 -0600

    factorial con recursividad

---
+++ factorial.js
@@ -0,0 +1,13 @@
+// Función factorial recursiva
+function factorial(n) {
+  if (n < 0) {
+    return "No existe factorial para números negativos";
+  }
+  if (n === 0 || n === 1) {
+    return 1; // Caso base
+  }
+  return n * factorial(n - 1); // Llamada recursiva
+}
+
+// Ejemplo de uso
+console.log(factorial(5)); // Resultado: 120
